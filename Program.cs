c03d6288feab8a80e6861bb8c58e8bbbfa5a6dba
shadlol1 <alzhranish690@gmail.com>
2026-07-01 18:12:38 +0300

    Update app views and navigation

--- Program.cs
+++ Program.cs
@@ -18,7 +18,11 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
+
 app.UseRouting();
 
 app.UseAuthorization();
